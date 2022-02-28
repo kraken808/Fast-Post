96bdfa7fb5155e68b488fb00d9661f0f3f9f4b25
kraken808 <murat99.04@gmail.com>
2022-02-28 10:09:37 +0600

    Initial Commit

---
+++ Fast-Post/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fast-Post
+//
+//  Created by  kraken808 on 28.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fast-Post/Fast_PostApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fast_PostApp.swift
+//  Fast-Post
+//
+//  Created by  kraken808 on 28.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Fast_PostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
